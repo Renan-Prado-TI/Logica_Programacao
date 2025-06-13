d69c3e14ddab0ebe57f5faa9ed24a70ec0108515
Renan-Prado-TI <renan.pradoti@outlook.com>
2025-06-13 08:57:19 -0300

    Subindo a aula 09

---
+++ Aula_09/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var promptSync = require("prompt-sync");
+var prompt = promptSync();
+var numero1 = parseInt(prompt("Digite o primeiro numero: "));
+var numero2 = parseInt(prompt("Digite o segundo numero: "));
+var numero3 = parseInt(prompt("Digite o terceiro numero: "));
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log("O maior numero e: " + numero1);
+}
+else if (numero2 > numero1 && numero2 > numero3) {
+    console.log("O maior numero e: " + numero2);
+}
+else if (numero3 > numero1 && numero3 > numero2) {
+    console.log("O maior numero e: " + numero3);
+}
+else {
+    console.log("Os numeros sao iguais");
+}
